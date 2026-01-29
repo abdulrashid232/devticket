4b981545a99b21155cb2834d95bd6350765d862a
Jude Boachie <jude.boachie@amalitech.com>
2026-01-29 15:38:45 +0000

    Add outline and outlint-flat variants

--- devticket-FE/src/app/shared/components/button/button.model.ts
+++ devticket-FE/src/app/shared/components/button/button.model.ts
@@ -1,2 +1,2 @@
-export type ButtonVariant = 'primary';
+export type ButtonVariant = 'primary' | 'outline' | 'outline-flat';
 export type ButtonSize = 'sm' | 'md' | 'lg';
--- devticket-FE/src/app/shared/components/button/button.ts
+++ devticket-FE/src/app/shared/components/button/button.ts
@@ -18,20 +18,24 @@ export class Button {
 
   public get buttonClasses(): string {
     const baseClasses =
-      'inline-flex items-center justify-center font-medium rounded-lg transition-all duration-150 ease-in-out focus:outline-none disabled:cursor-not-allowed border';
+      'inline-flex items-center justify-center font-medium text-white transition-all duration-150 ease-in-out focus:outline-none cursor-pointer disabled:cursor-not-allowed';
 
     const variantClasses: Record<ButtonVariant, string> = {
       primary:
-        'bg-[var(--primary-500)] border-[var(--primary-400)]/50 text-white shadow hover:bg-[var(--primary-600)] focus-visible:shadow-[0_0_0_3px_var(--primary-300)] active:bg-[var(--primary-700)] active:shadow-none disabled:bg-[var(--primary-300)] disabled:border-[var(--primary-300)]',
+        'border bg-[var(--primary-500)] border-[var(--primary-400)]/40 shadow hover:bg-[var(--primary-600)] focus-visible:shadow-[0_0_0_3px_var(--primary-300)] active:bg-[var(--primary-700)] active:shadow-none active:translate-y-px disabled:bg-[var(--primary-300)] disabled:border-[var(--primary-300)]',
+      outline:
+        'bg-neutral-800 hover:bg-neutral-800/85 border border-neutral-700 shadow active:shadow-none active:bg-neutral-800/50 active:translate-y-px',
+      'outline-flat':
+        'bg-transparent hover:bg-neutral-800/85 border border-neutral-700 shadow active:shadow-none active:bg-neutral-800/50 active:translate-y-px',
     };
 
     const sizeClasses: Record<ButtonSize, string> = {
-      sm: 'px-3 py-1.5 text-sm',
-      md: 'px-4 py-2 text-base',
-      lg: 'px-6 py-3 text-lg',
+      sm: 'px-3 py-1.5 text-sm rounded-md',
+      md: 'px-4 py-2 text-base rounded-lg',
+      lg: 'px-6 py-3 text-lg rounded-xl',
     };
 
-    const widthClass = this.fullWidth() ? 'w-full' : '';
+    const widthClass: string = this.fullWidth() ? 'w-full' : '';
 
     return `${baseClasses} ${variantClasses[this.variant()]} ${sizeClasses[this.size()]} ${widthClass}`.trim();
   }
