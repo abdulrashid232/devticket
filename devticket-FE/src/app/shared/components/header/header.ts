d54db4aa48f84678f92bb1a791a1969bbb4f01c2
Broderick Nana Bentil <bentilfrederick4@gmail.com>
2026-02-03 16:22:20 +0000

    feat(header): update user profile to use icon in the public folder

--- devticket-FE/src/app/shared/components/header/header.ts
+++ devticket-FE/src/app/shared/components/header/header.ts
@@ -7,7 +7,6 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
   selector: 'app-header',
   imports: [InputUi, Button, ReactiveFormsModule],
   templateUrl: './header.html',
-  styleUrl: './header.css',
 })
 export class Header {
   private formBuilder = inject(FormBuilder);
@@ -17,7 +16,5 @@ export class Header {
 
   public userName = signal('Abdul Musah');
   public position = signal('Administrator');
-  public userProfileImage = signal(
-    'https://lh3.googleusercontent.com/aida-public/AB6AXuBCBml1iVWHpkHdRr_ruQrrjA8o2v2PRwYnEHT69AzcMEWxbUJonDxyd7DTo2A3bA_hYwEkyimzTi3fQXdsBk1wph4IE9Gv6BsRX5k63pNFeJufiy8HYEj-9d1Xcjpjpe5Vx2-zV0pmcFykbDEjhOze0nIDh7yRmXnjO4GmS3emMgx8zyd7uZsmMtASL_8bIQ2a3VyvfsCsNR64FwyJbJD7t4mp7GEdrcLKGp6ihBJP8SIvq32ccQeQuBYsiYbfjN4ue6cCcyBmhTil',
-  );
+  public userProfileImage = signal('/assets/images/user-profile.png');
 }
